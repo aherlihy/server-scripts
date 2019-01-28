7754fab51cc796e3c38746aee30e087897557560
aherlihy <anna.herlihy@10gen.com>
2019-01-28 17:12:23 +0100

    update to localhost

--- rscs_shards.sh
+++ rscs_shards.sh
@@ -26,11 +26,11 @@ trap clean_up SIGHUP SIGINT SIGTERM
 rm -rf $DBPATH && mkdir -p "$CPATH/configdb-0" "$CPATH/configdb-1" "$CPATH/configdb-2" "$SPATH/rs$RS-0" "$SPATH/rs$RS-1" "$SPATH/rs$RS-2"
 
 # Start config repl set
-$SERVER_VERSION/mongod --port $C_PORT --dbpath=$CPATH"/configdb-0" --replSet="rs"$RS --configsvr &> /dev/null &
+$SERVER_VERSION/mongod --port $C_PORT --dbpath=$CPATH"/configdb-0" --replSet="rs"$RS --configsvr &
 S1=$!
-$SERVER_VERSION/mongod --port $(($C_PORT+1)) --dbpath=$CPATH"/configdb-1" --replSet="rs"$RS --configsvr &> /dev/null &
+$SERVER_VERSION/mongod --port $(($C_PORT+1)) --dbpath=$CPATH"/configdb-1" --replSet="rs"$RS --configsvr &
 S2=$!
-$SERVER_VERSION/mongod --port $(($C_PORT+2)) --dbpath=$CPATH"/configdb-2" --replSet="rs"$RS --configsvr &> /dev/null &
+$SERVER_VERSION/mongod --port $(($C_PORT+2)) --dbpath=$CPATH"/configdb-2" --replSet="rs"$RS --configsvr &
 S3=$!
 
 echo "S1=$S1 S2=$S2 S3=$S3"
@@ -38,20 +38,20 @@ echo "S1=$S1 S2=$S2 S3=$S3"
 sleep 2
 
 cfg="{
-    _id: \"rs$RS\",
-    members: [
-        {_id: 0, host: 'auckland.local:$C_PORT'},
-        {_id: 1, host: 'auckland.local:$(($C_PORT+1))'},
-        {_id: 2, host: 'auckland.local:$(($C_PORT+2))'}
+    \"_id\": \"rs$RS\",
+    \"members\": [
+        {\"_id\": 0, \"host\": \"localhost:$C_PORT\"},
+        {\"_id\": 1, \"host\": \"localhost:$(($C_PORT+1))\"},
+        {\"_id\": 2, \"host\": \"localhost:$(($C_PORT+2))\"}
     ],
-    configsvr:true
+    \"configsvr\":true
 }"
 echo "Initiating config with $cfg"
 $SERVER_VERSION/mongo --port $C_PORT --eval "JSON.stringify(rs.initiate($cfg))"
 sleep 2
 
 # Start mongos
-$SERVER_VERSION/mongos --port $PORT --configdb "rs"$RS/auckland.local:$C_PORT,auckland.local:$(($C_PORT+1)),auckland.local:$(($C_PORT+2)) &
+$SERVER_VERSION/mongos --port $PORT --configdb "rs"$RS/localhost:$C_PORT,localhost:$(($C_PORT+1)),localhost:$(($C_PORT+2)) &
 S4=$!
 echo "S4=$S4"
 
@@ -70,15 +70,15 @@ sleep 2
 cfg="{
     _id: \"rs$SRS\",
     members: [
-        {_id: 0, host: 'auckland.local:$S_PORT'},
-        {_id: 1, host: 'auckland.local:$(($S_PORT+1))'},
-        {_id: 2, host: 'auckland.local:$(($S_PORT+2))'}
+        {_id: 0, host: 'localhost:$S_PORT'},
+        {_id: 1, host: 'localhost:$(($S_PORT+1))'},
+        {_id: 2, host: 'localhost:$(($S_PORT+2))'}
     ]
 }"
 echo "Initiating repl shard with $cfg"
 $SERVER_VERSION/mongo --port $S_PORT --eval "JSON.stringify(rs.initiate($cfg))"
 
 sleep 10
-$SERVER_VERSION/mongo --port $PORT --eval "JSON.stringify(sh.addShard(\"rs$SRS/auckland.local:$(($S_PORT))\"))"
+$SERVER_VERSION/mongo --port $PORT --eval "JSON.stringify(sh.addShard(\"rs$SRS/localhost:$(($S_PORT))\"))"
 
 cat
